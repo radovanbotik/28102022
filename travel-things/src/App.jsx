6c288526153902c4d92fb69b322bed05db31abcf
radovanbotik <radovanbotik@gmail.com>
2022-10-28 16:15:48 +0200

    ''

---
+++ travel-things/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./components/Header";
+import Log from "./components/Log";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Log />
+    </div>
+  );
+}
+
+export default App;
