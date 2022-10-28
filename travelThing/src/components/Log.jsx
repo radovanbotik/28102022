f962c18afffb905eba4eb90faf9c22ddb58a5817
radovanbotik <radovanbotik@gmail.com>
2022-10-28 16:43:52 +0200

    ''

--- travelThing/src/components/Log.jsx
+++
@@ -1,9 +0,0 @@
-import Event from "./Event";
-import TravelData from "../../public/data";
-
-export default function Log() {
-  const events = TravelData.map((entry, index) => {
-    return <Event key={index} {...entry} />;
-  });
-  return <div className="log">{events}</div>;
-}
